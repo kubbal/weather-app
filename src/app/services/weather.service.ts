2af3e5977668395b0543fc9f00305f1ff4231759
kubbal <kublerbalazs@gmail.com>
2023-10-10 16:33:12 +0200

    Make API call return WeatherResponseDto

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { WeatherResponseDto } from '../models/WeatherResponseDto';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,6 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getWeatherData(location: string) {
-    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${environment.apiKey}`);
+    return this.http.get<WeatherResponseDto>(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${environment.apiKey}`);
   }
 }
